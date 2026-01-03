2b9f95fc9acdd0dba31bdfc5cb34303aa9c781cd
Kazuki Nakagawa <s24c1089eq@s.chibakoudai.jp>
2026-01-03 19:02:02 +0900

    Add launch and service

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,15 +1,29 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16
-
-def cb(msg):
-    global node
-    node.get_logger().info("Listen: %d" % msg.data)
+from person_msgs.srv import Query
 
 def main():
-    global node
     rclpy.init()
     node = Node("listener")
-    pub = node.create_subscription(Int16, "countup", cb, 10)
-    rclpy.spin(node)
 
+    client = node.create_client(Query, 'query') 
+    while not client.wait_for_service(timeout_sec=1.0):
+        node.get_logger().info('待機中...')
+
+    req = Query.Request()
+    req.name = "kazu624"
+    future = client.call_async(req)
+
+    while rclpy.ok():
+        rclpy.spin_once(node)
+        if future.done():
+            try:
+                response = future.result()
+            except:
+                node.get_logger().info('呼び出し失敗')
+            else:
+                node.get_logger().info("age: {}".format(response.age))
+            break
+
+    node.destroy_node()
+    rclpy.shutdown()
--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,19 +1,17 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16
+from person_msgs.srv import Query
 
-rclpy.init()
-node = Node("talker")
-pub = node.create_publisher(Int16, "countup", 10)
-n = 0
+def cb(request, response):
+    if request.name == "kazu624":
+        response.age = 22
+    else:
+        response.age = 255
 
-def cb():
-    global n
-    msg = Int16()
-    msg.data = n
-    pub.publish(msg)
-    n += 1
+    return response
 
 def main():
-    node.create_timer(0.5, cb)
+    rclpy.init()
+    node = Node("talker")
+    srv = node.create_service(Query, "query", cb) # サービスを作成
     rclpy.spin(node)
