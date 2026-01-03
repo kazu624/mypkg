abf63db9048c366b14e1fc461dc35f017ca3bbff
Kazuki Nakagawa <s24c1089eq@s.chibakoudai.jp>
2026-01-03 20:37:55 +0900

    Add ROS2 system source

--- test/test.bash
+++ test/test.bash
@@ -4,7 +4,11 @@ dir=~
 [ "$1" != "" ] && dir="$1"
 
 cd $dir/ros2_ws
+
+source /opt/ros/humble/setup.bash
+
 colcon build
+
 source install/setup.bash
 
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
